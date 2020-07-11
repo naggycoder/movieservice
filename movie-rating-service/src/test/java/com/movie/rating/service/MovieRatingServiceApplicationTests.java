25425a453e31cc3c2b146600855d82f127d1fd59
naggycoder <naggy.coder@mail.com>
2020-07-11 22:12:03 +0530

    Initial checkin for Movie Rating Service

---
+++ movie-rating-service/src/test/java/com/movie/rating/service/MovieRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.rating.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
